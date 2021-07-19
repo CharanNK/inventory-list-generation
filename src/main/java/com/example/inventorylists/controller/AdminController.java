22ed6384f0d4d1e40883cf4e570c9e68bac91853
“charan” <charkeet@gmail.com>
2021-07-19 16:27:32 +0530

    establishing mysqlconnection. closes #2

---
+++ src/main/java/com/example/inventorylists/controller/AdminController.java
@@ -0,0 +1,12 @@
+package com.example.inventorylists.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+    @RequestMapping("/sayHello")
+    public String sayHello(){
+        return "Hello World!";
+    }
+}
