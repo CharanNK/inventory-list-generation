61f440cb4f26c2c5470869429ae4bf4e2820bdac
“charan” <charkeet@gmail.com>
2021-07-19 15:13:30 +0530

    Initial project setup. closes #1

---
+++ src/main/java/com/example/inventorylists/InventoryListsApplication.java
@@ -0,0 +1,13 @@
+package com.example.inventorylists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryListsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryListsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/inventorylists/InventoryListsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inventorylists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryListsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
